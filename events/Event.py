985c9a706c88c547f360f5980940b90e8db9d587
Joseph Mancuso <idmann509@gmail.com>
2018-07-01 11:16:28 -0400

    added better event firing

--- events/Event.py
+++ events/Event.py
@@ -1,5 +1,6 @@
 class Event:
     listeners = {}
+    _fired_events = {}
 
     def __init__(self, container):
         self.container = container
@@ -12,7 +13,31 @@ class Event:
         self.listeners.update({event: listeners})
         return self
     
+    # TODO: Add wildcard like self.fire('user.*')
     def fire(self, events):
-        for event in self.listeners[events]:
-            event = self.container.resolve(event)
-            self.container.resolve(event.handle)
+        fired_listeners = {}
+        if isinstance(events, str) and '*' in events:
+            for event_action, listener_events in self.listeners.items():
+                fired_listeners.update({event_action: []})
+                for listener in listener_events:
+                    search = events.split('*')
+                    if events.endswith('*') and event_action.startswith(search[0]) \
+                    or events.startswith('*') and event_action.endswith(search[1]) \
+                    or event_action.startswith(search[0]) and event_action.endswith(search[1]):
+                        fired_listeners[event_action].append(listener)
+                        event = self.container.resolve(listener)
+                        self.container.resolve(event.handle)       
+        else:
+            for event in self.listeners[events]:
+                event = self.container.resolve(event)
+                self.container.resolve(event.handle)
+
+        self._fired_events = self.clear_blank_fired_events(fired_listeners)
+    
+    def clear_blank_fired_events(self, fired_listeners):
+        new_dictionary = {}
+        for event, listeners in fired_listeners.items():
+            if listeners:
+                new_dictionary.update({event: listeners})
+
+        return new_dictionary
--- tests/test_events.py
+++ tests/test_events.py
@@ -14,9 +14,25 @@ class TestEvent:
 
     def setup_method(self):
         self.app = App()
-        self.app.bind('Event', Event(self.app))
-    
+        event = Event(self.app)
+
+        self.app.bind('Event', event)
+    def test_fire_event_with_wildcard_ends_with(self):
+        events = self.app.make('Event').listen('user.registered', [
+            EventListener, EventListener
+        ])
+
+        events = self.app.make('Event').listen('user.subscribed', [
+            EventListener
+        ])
+
+        event = self.app.make('Event')
+
+        assert event.fire('*.registered') is None
+        assert event._fired_events == {'user.registered': [EventListener, EventListener]}
+
     def test_add_listener(self):
+        self.app.make('Event').listeners = {}
         events = self.app.make('Event').listen(UserAddedEvent, [
             EventListener
         ])
@@ -33,13 +49,32 @@ class TestEvent:
         ])
 
         assert events.fire(UserAddedEvent) is None
-    
-    def test_fire_event_with_string(self):
-        listeners = []
-        for i in range(1, 500):
-            listeners.append(EventListener)
 
-        event = self.app.make('Event').listen('user.registered', listeners)
+    def test_fire_event_with_wildcard_starts_with(self):
+        self.app.make('Event').listeners = {}
+        events = self.app.make('Event').listen('user.registered', [
+            EventListener
+        ])
+        events = self.app.make('Event').listen('user.subscribed', [
+            EventListener
+        ])
 
-        assert event.fire('user.registered') is None
+        event = self.app.make('Event')
+
+        assert event.fire('user.*') is None
+        assert event._fired_events == {'user.registered': [EventListener], 'user.subscribed': [EventListener]}
+
+    def test_fire_event_with_wildcard_in_middle_of_fired_event(self):
+        self.app.make('Event').listeners = {}
+        events = self.app.make('Event').listen('user.manager.registered', [
+            EventListener
+        ])
+        events = self.app.make('Event').listen('user.owner.subscribed', [
+            EventListener
+        ])
+
+        event = self.app.make('Event')
+
+        assert event.fire('user.*.registered') is None
+        assert event._fired_events == {'user.manager.registered': [EventListener]}
 
